2e05f907d1c04e7e5f910ddb6660d2e55e7d97d9
Rebecca YY <yiny5@student.unimelb.edu.au>
2021-03-29 17:45:41 +1100

    Add files via upload

---
+++ Nonconst-WH_BH/Multiscale_hybrid_1.m
@@ -0,0 +1,208 @@
+function Multiscale_hybrid_1()
+
+% For 'Multiscale_hybrid_1', we assume that the within-host subsystem has
+% reached its steady states, i.e. we have constant W-H subsystem. 
+
+clc
+clear
+
+[~, ~, T_realtime, infect_timepoint] = CTMC_BH_driver(); 
+% Note that 'infect_timepoint' is an array recording the timestamp where a
+% new infection happens. 
+
+% Parameter of interest in terms of real time, t: 
+THETA_I = zeros(size(T_realtime)) * nan; 
+
+% We firstly assume that our Within-Host subsystem is constant:
+[T_SS, ~, V_SS] = const_WH();
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Interpolation to obatain two piecewise polynomials: 
+%[t_WH, T, ~, V] = Determ_WH_driver();
+%T_poly = spline(t_WH,T); V_poly = spline(t_WH,V); %%% Try linear interpolation as well %%%!!!!!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+for i = 1 : length(T_realtime)
+    
+    % Record the infectious time length (note that only non-negative
+    % numericla entries are corresponeded to the infectious groups: 
+    infect_length = ones(length(infect_timepoint), 1) * T_realtime(i) - infect_timepoint';
+    
+    % Initialisation: 
+    sum_LHS = 0;
+    T_sum = 0;
+    V_sum = 0;
+    num_infectious = 0;
+    
+    for j = 1 : length(infect_length)
+        if infect_length(j) >= 0 % Only those entries make sense
+            num_infectious = num_infectious + 1;
+            Vj = V_SS; 
+            Tj = T_SS;
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %Vj = ppval(V_poly, infect_length(j));
+            %Tj = ppval(T_poly, infect_length(j));
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            sum_LHS = sum_LHS + Vj * Tj;
+            
+            T_sum = T_sum + Tj;
+            V_sum = V_sum + Vj;
+        end
+    end
+    
+    T_ave = T_sum / num_infectious;
+    V_ave = V_sum / num_infectious;
+    
+    
+    THETA_I(i) = sum_LHS / (T_ave * V_ave);
+end
+
+%plot(T_realtime, THETA_I, 'LineWidth', 1.2); hold on
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Constant Within-Host Subsystem: 
+function [T_SS, Tstar_SS, V_SS] = const_WH()
+
+% One can run 'Determ_WH_driver.m' to check that under these values, our
+% Within-Host subsystem arrives at a non-virus free equilibrium. 
+p = 10 ^ (1);
+c = 10 ^ (0);
+k = 10 ^ (-7);
+Lambda = 10 ^ (10);
+mu_c = 10 ^ (-1);
+delta_c = 10 ^ (1);
+
+T_SS = (mu_c + delta_c) * c / (p * k);
+Tstar_SS = ((Lambda * p * k) / (c * (mu_c + delta_c)) - mu_c) * c / (p * k);
+V_SS = ((Lambda * p * k) / (c * (mu_c + delta_c)) - mu_c) / k;
+
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Between-Host Subsystem (Note that transmission coefficient is proportinoal 
+% to viral load times a contant, beta0):
+function [S, I, t, infect_timepoint] = CTMC_BH(mu, Lambda, t_end, population, V_SS)
+
+% CTMS_BH simulation
+
+S = []; S(1) = population - 10;
+I = []; I(1) = population - S(1);
+t = []; t(1) = 0;
+infect_timepoint = [];
+for i = 1 : I(1)
+    infect_timepoint(i) = 0;
+end
+index = 1;
+
+% We assume that beta = beta0 * V_SS as presented in the research paper
+beta0 = 1 / (10 ^ 13);
+beta = beta0 * V_SS; % Link the Between Host subsystem to the Within Host subsystem
+
+while t(index) <= t_end
+    lambda = beta * I(index) * S(index) + mu * S(index) + mu * I(index) + Lambda;
+    Delta_t = TimeStepSize(lambda);
+    t(index + 1) = t(index) + Delta_t;
+    
+    [S, I, infectious_increase, infectious_dies] = EventHappen(lambda, beta, mu, S, I, index);
+    
+    % records the time stamp where a new infection happens
+    if infectious_increase == 1
+        infect_timepoint(I(1) + index) = t(index); 
+    else
+        infect_timepoint(I(1) + index) = nan;
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % The variable, 'infectious_dies', records the time stamp where an infectius individual decays(/dies)
+    % However, how shall we decide which infectious person to be
+    % removed from the system????????????????????????????????????????????
+    % The below codes randomly select one infectious individual and remote
+    % it from the system.
+    if infectious_dies == 1
+        infectious_people = find(infect_timepoint >= 0); % Indecies of infectious people
+        rand_die = randi([1, length(infectious_people)], 1, 1);
+        infect_timepoint(infectious_people(rand_die)) = nan;
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    index = index + 1;
+ 
+end
+
+if t(length(t)) > t_end
+    t = t(1 : length(t) - 1);
+    S = S(1 : length(S) - 1);
+    I = I(1 : length(I) - 1);
+end
+
+plot_SI(S, I, t)
+% legend('S_{CTMC}', 'I_{CTMC}');
+
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function Delta_t = TimeStepSize(lambda)
+
+u1 = rand;
+Delta_t = -log(u1) / lambda;
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [S, I, infectious_increase, infectious_dies] = EventHappen(lambda, beta, mu, S, I, index)
+
+u2 = rand;
+p1 = beta * S(index) * I(index) /lambda;
+p2 = mu * S(index) / lambda; 
+p3 = mu * I(index) / lambda ;
+
+infectious_increase = 0;
+infectious_dies = 0;
+
+if u2 >= 0 && u2 < p1 % One person gets infectious
+    S(index + 1) = S(index) - 1;
+    I(index + 1) = I(index) + 1;
+    infectious_increase = 1;
+elseif u2 >= p1 && u2 < p1 + p2 % One susceptible dies
+    S(index + 1) = S(index) - 1;
+    I(index + 1) = I(index);
+elseif u2 >= p1 + p2 && u2 < p1 + p2 + p3 % One infectious dies
+    S(index + 1) = S(index);
+    I(index + 1) = I(index) - 1;
+    infectious_dies = 1; 
+else % One susceptible enters the population
+    S(index + 1) = S(index) + 1;
+    I(index + 1) = I(index);
+end
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function plot_SI(S, I, t)
+    plot(t, S,'b', t, I, 'k', 'LineWidth', 2); hold on;
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [S, I, t, infect_timepoint] = CTMC_BH_driver()
+
+% Initialise the parameters for the Between-Host system:
+mu = 10 ^ (-5);
+Lambda = 10 ^ (-5);
+%t_end = 300; population = 10 ^ (5);
+population = 10 ^ (2); t_end = 100;
+[~, ~, V_SS] = const_WH();
+
+[S, I, t, infect_timepoint] = CTMC_BH(mu, Lambda, t_end, population, V_SS);
+
+end
---
+++ Nonconst-WH_BH/hybrid_simulation_Ver2.m
@@ -0,0 +1,336 @@
+function hybrid_simulation_Ver2(S0, I0, Lambda, mu, C0, beta0, t_end)
+
+global Sdie Idie Infect_timepoint infect_timelength Q C S I T_poly V_poly t_array
+
+Sdie = []; Idie = []; Infect_timepoint = []; infect_timelength = []; 
+Q = []; C =[]; S = []; S(1) = S0; I = []; I(1) = I0; t_array = []; t_array(1) = 0;
+
+% Initialise all the arrays needed
+t0 = 0; init_system(Lambda, mu, t0, C0); t_index = 1;
+
+% Solve for the W-H system once: 
+[t_WH, T, ~, V] = Determ_WH_driver();
+T_poly = interp1(t_WH,T, 'linear','pp'); V_poly = interp1(t_WH,V, 'linear','pp');
+
+while t_array(length(t_array)) < t_end
+    if ~ isempty(Q)
+        event(t_index, mu, Lambda, beta0, C0); 
+        t_index = t_index + 1;
+    else
+        % Regenerate the queue Q: 
+        regenerate_Q(Lambda, mu, C0);
+    end
+end
+
+% Make sure that t < t_end
+if t_array(length(t_array)) > t_end
+    t_array = t_array(1 : length(t_array) - 1);
+    S = S(1 : length(t_array));
+    I = I(1 : length(t_array));
+end
+
+plot(t_array, S, 'g', t_array, I, 'r', 'LineWidth', 1.2); hold on
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Debugged!
+function init_system(Lambda, mu, t0, C0)
+
+global Sdie Idie Infect_timepoint infect_timelength Q C S I
+
+% The waiting time for 1 susceptible to enter the population: 
+u1 = rand;
+T1 = -log(u1) / Lambda; 
+
+% Create an array, 'Sdie', to store the waiting time for each susceptible
+% to die:
+for i = 1 : S(length(S))
+    u2 = rand;
+    Sdie(i) = -log(u2) / mu;
+end
+
+% Create an array, 'Idie', to store the waiting time for each infectious
+% to die:
+for i = 1 : I(length(I))
+    u3 = rand;
+    Idie(i) = -log(u3) / mu;
+end
+
+% Create an array, 'Infect_timepoint', to record the time stamp at which
+% each infectious person gets infectious:
+for i = 1 : I(length(I))
+    Infect_timepoint(i) = t0;
+end
+
+% Create another array, 'infect_timelength', to record the time length for
+% which each infectious person stays infectious:
+for i = 1 : I(length(I))
+    infect_timelength(i) = t0 - t0;
+end
+
+% Create an array, 'C', to record the waiting time for each infectious
+% individual to interact with a susceptible person: 
+for i = 1 : I(length(I))
+    u4 = rand;
+    C(i) = -log(u4) / (C0 * S(length(S)));
+end
+
+% Create the event queues, 'Q': 
+Q = [C, Sdie, Idie, T1];
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CHECK FURTHER FOR LOGIC ERRORS!!!!!!!!!!!!!!!
+function event(t_index, mu, Lambda, beta0, C0)
+
+global Q S I C Sdie Idie V_poly infect_timelength Infect_timepoint t_array T1
+
+[~, ~, V_SS] = const_WH();
+
+% Find the minimal value in the event queue Q:
+[DeltaT, index] = min(Q);
+
+% If a new susceptible will enter the population:
+if index == length(Q)
+    S(t_index + 1) = S(t_index) + 1; I(t_index + 1) = I(t_index); 
+    
+    % Update 'Sdie', 'C', and 'infect_timelength': 
+    Sdie(S(t_index + 1)) = -log(rand) / mu;
+    C = update_C(S, I, C, C0);
+    infect_timelength = infect_timelength + DeltaT;
+    
+    % Add a new T1 into Q and update Q:
+    T1_new = -log(rand) / Lambda; 
+    Q = [C, Sdie, Idie, T1_new];
+end
+
+% If some susceptible person dies: 
+if (length(Idie) + 1 <= index) && (index <= length(Idie) + length(Sdie))
+    S(t_index + 1) = S(t_index) - 1; I(t_index + 1) = I(t_index); 
+    
+    % Remove that susceptible from the system and update 'C' and 'infect_timelength': 
+    j = index - length(Idie);
+    Sdie = delete(j, Sdie);
+    C = update_C(S, I, C, C0);
+    infect_timelength = infect_timelength + DeltaT;
+    
+    Q = [C, Sdie, Idie, T1];
+end
+
+% If some infectious person dies: 
+if (length(Idie) + length(Sdie) + 1 <= index) && (index <= 2 * length(Idie) + length(Sdie))
+    S(t_index + 1) = S(t_index); I(t_index + 1) = I(t_index) - 1; 
+    
+    % Remove that infectious from the system: 
+    j = index - length(Idie) - length(Sdie);
+    Idie = delete(j, Idie);
+    C = delete(j, C);
+    Infect_timepoint = delete(j, Infect_timepoint);
+    infect_timelength = delete(j, infect_timelength);
+    
+    infect_timelength = infect_timelength + DeltaT;
+    
+    Q = [C, Sdie, Idie, T1];
+end 
+
+% If an infectious person i=index interacts with a susceptible person
+if (1 <= index) && (index <= length(C))
+    V_1 = ppval(V_poly, infect_timelength(index));
+    if V_1 < 0
+        V_1 = V_SS;
+    end
+    TV = beta0 * V_1; % is TV in [0, 1]?????????????????????????????? 
+    u5 = rand;
+    
+    if u5 >= TV
+        % A susceptible becomes infectious
+        S(t_index + 1) = S(t_index) - 1; I(t_index + 1) = I(t_index) + 1; 
+        
+        % The susceptible is randomly selected and deleted from the group
+        % of susceptible people: 
+        Sj = randi(S(t_index));
+        Sdie = delete(Sj, Sdie);
+        
+        % Add the new infectious individual to the system:
+        new_death_time = -log(rand) / mu;
+        Idie(length(Idie) + 1) = new_death_time;
+        % Update information
+        infect_timelength = infect_timelength + DeltaT;
+        Infect_timepoint(I(t_index + 1)) = t_array(t_index) + DeltaT;
+        infect_timelength(length(infect_timelength) + 1) = 0;
+        C = update_C(S, I, C, C0);
+    else
+        % Nonthing happens
+        S(t_index + 1) = S(t_index); I(t_index + 1) = I(t_index); 
+        infect_timelength = infect_timelength + DeltaT;
+    end
+    
+    Q = [C, Sdie, Idie, T1];
+end
+
+% Increment in time: 
+t_index  = t_index + 1;
+t_array(t_index) = t_array(t_index - 1) + DeltaT;
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Debugged!
+function array = delete(index, array)
+
+% Remove that the 'index_th' entry in the array
+if length(array) == 1
+    array = [];
+else
+    if index > 1 && index < length(array)
+        array = [array(1 : index - 1), array(index + 1 : length(array))];
+    else
+        if index == 1
+            array = array(index + 1 : length(array));
+        elseif index == length(array)
+            array = array(1 : length(array) - 1);
+        end
+    end
+end
+    
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Debugged!
+function C = update_C(S, I, C, C0)
+
+for i = 1 : I(length(I))
+    u4 = rand;
+    C(i) = -log(u4) / (C0 * S(length(S)));
+end
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Debugged!
+function regenerate_Q(Lambda, mu, C0)
+
+global Sdie Idie Q C S I
+
+% The waiting time for 1 susceptible to enter the population: 
+u1 = rand;
+T1 = -log(u1) / Lambda; 
+
+% Create an array, 'Sdie', to store the waiting time for each susceptible
+% to die:
+for i = 1 : S(length(S))
+    u2 = rand;
+    Sdie(i) = -log(u2) / mu;
+end
+
+% Create an array, 'Idie', to store the waiting time for each infectious
+% to die:
+for i = 1 : I(length(I))
+    u3 = rand;
+    Idie(i) = -log(u3) / mu;
+end
+
+% Create an array, 'C', to record the waiting time for each infectious
+% individual to interact with a susceptible person: 
+for i = 1 : I(length(I))
+    u4 = rand;
+    C(i) = -log(u4) / (C0 * S(length(S)));
+end
+
+% Create the event queues, 'Q': 
+Q = [C, Sdie, Idie, T1];
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Debugged!
+% Deterministic Within-Host Subsystem: 
+function [t, T, Tstar, V] = Determ_WH(k, c, p, mu_c, delta_c, Lambda, T0, Tstar0, V0, t_end)
+
+% I perform the log transformation to the W-H system: 
+y0 = [log(T0); log(Tstar0); log(V0)];
+tspan = [0, t_end];
+opts = odeset('RelTol',1e-8,'AbsTol',1e-8);
+[t, y] = ode15s(@(t,y) ode_sys(t, y, k, c, p, mu_c, delta_c, Lambda), tspan, y0, opts);
+T_Log = y(: , 1); T = exp(T_Log);
+Tstar_Log = y(: , 2); Tstar = exp(Tstar_Log);
+V_Log = y(: , 2); V = exp(V_Log);
+
+
+%plot_WH(T, Tstar, V, t); % NOote that Tstar(t) and V(t) almost overlap with each other. 
+%xlabel('t'); ylabel('cell population');
+%title('Numerical Simulation (Deterministic) for Within-Host Subsystem');
+
+end
+
+
+
+function dydt = ode_sys(t, y, k, c, p, mu_c, delta_c, Lambda)
+
+    dydt = [Lambda * exp(-y(1)) - k * exp(y(3)) - mu_c;
+            k * exp(y(1) + y(3) - y(2)) - mu_c - delta_c;
+            -c + p * exp(y(2) - y(3))];
+end
+
+
+function plot_WH(T, Tstar, V, t)
+    plot(t, T, t, Tstar, t, V, 'LineWidth', 1.5); 
+    legend('T(t)', 'T^{*}(t)', 'V(t)');
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Debugged!
+function [t, T, Tstar, V] = Determ_WH_driver()
+
+% The magnitute of parameters is drawn from  the research paper: 
+% 'The Mechanisms for Within-Host Influenza Virus Control Affect 
+% Model-Based Assessment and Prediction of Antiviral Treatment'
+T0 = 10 ^ (8);
+Tstar0 = 1;
+V0 = 10 ^ (4);
+p = 10 ^ (1);
+c = 10 ^ (0);
+k = 10 ^ (-7);
+Lambda = 10 ^ (-1);
+mu_c = 10 ^ (-1);
+delta_c = 10 ^ (1);
+
+t_end = 10 ^ (0.6); % One can see that the W-H subsystem timescale is 
+                    % much shorter than that of B-H subsystem
+[t, T, Tstar, V] = Determ_WH(k, c, p, mu_c, delta_c, Lambda, T0, Tstar0, V0, t_end); 
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Debugged!
+% Constant Within-Host Subsystem: 
+function [T_SS, Tstar_SS, V_SS] = const_WH()
+
+% This function returns the non-virus-free steady state, 
+% '(T_SS, Tstar_SS, V_SS)', of the within-host subsytem.
+% 
+% One can run 'Determ_WH_driver.m' to check that under these values, our
+% Within-Host subsystem arrives at a non-virus free equilibrium. 
+
+% Parameter Initialisation:
+p = 10 ^ (1);
+c = 10 ^ (0);
+k = 10 ^ (-7);
+Lambda = 10 ^ (10);
+mu_c = 10 ^ (-1);
+delta_c = 10 ^ (1);
+
+% Compute the Steady States: 
+T_SS = (mu_c + delta_c) * c / (p * k);
+Tstar_SS = ((Lambda * p * k) / (c * (mu_c + delta_c)) - mu_c) * c / (p * k);
+V_SS = ((Lambda * p * k) / (c * (mu_c + delta_c)) - mu_c) / k;
+
+end
