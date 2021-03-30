82cbe4884abac6c9cb62d0345c05c2901aa32b68
Rebecca YY <yiny5@student.unimelb.edu.au>
2021-03-30 19:57:54 +1100

    Update hybrid_simulation_Ver2_driver.m

--- Nonconst-WH_BH/hybrid_simulation_Ver2_driver.m
+++ Nonconst-WH_BH/hybrid_simulation_Ver2_driver.m
@@ -4,11 +4,11 @@ S0 = 90;
 I0 = 10;
 Lambda = 10 ^ (-5);
 mu = 10 ^ (-5);
-C0 = 10 ^ (-3);
-t_end = 100;
+C0 = 10 ^ (-4);
+t_end = 800;
 
-rep = 100; 
-for j = 10 : 11
+rep = 20; 
+for j = 10 : 10
     figure(j)
     beta0 = 10 ^ (-j);
     for i = 1 : rep
@@ -16,7 +16,7 @@ for j = 10 : 11
         Multiscale_hybrid_1()
     end
     xlabel('time'); ylabel('population');
-    title(['Non-const W-H CTMC (red & green) VS Const W-H CTMC (black & blue), with C_0 = ', num2str(C0), ' and \beta_0 = ', num2str(beta0)]);
+    title(['Non-const W-H CTMC (red & green) VS Const W-H CTMC (black & blue), with C_0 = ', num2str(C0), ' and \beta_0 = ', num2str(beta0), ' (rep = ', num2str(rep), ' )']);
 end
 
 %Multiscale_hybrid_1()
